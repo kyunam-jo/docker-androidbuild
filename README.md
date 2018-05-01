db181a6763351800760587ea95624e4fd3b35480
Kyunam Jo <kyunam.jo@lge.com>
2018-05-02 07:47:28 +0900

    Fixed Port Number and added run command's guide in README

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # docker-androidbuild
 This image can build aosp and codeaurora source (with kernel) based on ubuntu
+
+# How to run
+run.sh [USER-ID]
--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@
 
 ((PORT_NUM=$(netstat -ntl | awk '{print $4}' | grep -E -o '222[0-9]+' | tail -1)+1))
 
-if [ -z $PORT_NUM ];then
-    PORT_NUM=2222
+if [ "$PORT_NUM" = "1" ];then
+    PORT_NUM=2221
 fi
 
 echo ">>>> Port Num is $PORT_NUM <<<<"
 
 mkdir -p ~/user/$1
 
-sudo docker run -e LOCAL_USER_ID=`id -u $USER` -e LOCAL_USER_NAME=$1 -p $PORT_NUM:22 -v $HOME/user/$1:/home/$1/source -v $HOME/.ccache:/home/$1/.ccache -v /home/mirror:/home/mirror -d -t mybase
+sudo docker run -e LOCAL_USER_ID=`id -u $USER` -e LOCAL_USER_NAME=$1 -p $PORT_NUM:22 -v $HOME/user/$1:/home/$1/source -v $HOME/.ccache:/home/$1/.ccache -v $HOME/docker:/home/$1/docker -v /home/mirror:/home/mirror -d -t helios30/android-build
